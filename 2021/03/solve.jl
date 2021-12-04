38283944d3e1d7eeb26e5f1085511ea63fa2c1ed
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-12-04 02:35:58 +0100

    Solve day 03b

--- 2021/03/solve.jl
+++ 2021/03/solve.jl
@@ -12,8 +12,7 @@ function bitmask(input)
     return mask'
 end
 
-function power(input)
-    mask = bitmask(input)
+function power(mask)
     m = size(mask, 1)
     γ₂ = vec(count(mask, dims=1) .> m÷2)
     reverse!(γ₂) # switch endianness
@@ -21,8 +20,36 @@ function power(input)
     # convert to decimal:
     γ = evalpoly(2, γ₂)
     ε = evalpoly(2, ε₂)
-    return γ*ε
+    return γ, ε, γ*ε
 end
 
-@test power("test.txt") == 198
-@show power("input.txt")
+function rating(R, data)
+    m, n = size(data)
+    mask = trues(m)
+    for i in 1:n
+        col = data[:,i]
+        bit = R(count(col .& mask), m/2)
+        mask .&= col .== bit
+        m = count(mask)
+        m == 1 && break
+    end
+    j = findfirst(mask)
+    bin = reverse(data[j,:])
+    dec = evalpoly(2, bin)
+    return dec
+end
+
+function lifesupport(data)
+    O2 = rating(>=, data)
+    CO2 = rating(<, data)
+    return O2, CO2, O2*CO2
+end
+
+test = bitmask("test.txt")
+input = bitmask("input.txt")
+
+@test power(test) == (22, 9, 198)
+@show power(input)
+
+@test lifesupport(test) == (23, 10, 230)
+@show lifesupport(input)
