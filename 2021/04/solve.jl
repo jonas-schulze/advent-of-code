aafee89cad180199c1667143b93b9855eb891144
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-12-06 00:41:52 +0100

    Solve day 04b

--- 2021/04/solve.jl
+++ 2021/04/solve.jl
@@ -33,7 +33,7 @@ function load(file)
     return drawn, cboards
 end
 
-function bingo(drawn, boards)
+function first_bingo(drawn, boards)
     marked = falses(size(boards))
     n = size(boards, 3)
     rows = Array{Int}(undef, 5, 1, n)
@@ -58,11 +58,52 @@ function bingo(drawn, boards)
     board = boards[:,:,b]
     nomarks = map(!, marked[:,:,b])
     s = sum(board[nomarks])
-    return s, num, s*num
+    return b, num, s, s*num
+end
+
+function last_bingo(drawn, boards)
+    @assert allunique(drawn)
+
+    # Compute the round a respective field was marked:
+    marked = zero(boards)
+    local num, b
+    for (i, num) in enumerate(drawn)
+        ids = findall(==(num), boards)
+        marked[ids] .= i
+    end
+
+    # Compute the round a respective board won:
+    function round_filled(d, never)
+        # Compute the round the last field of a respective row/col got marked,
+        # or `never` if the row/col still has unmarked fields:
+        notfull = minimum(marked, dims=d) .== 0
+        round = maximum(marked, dims=d)
+        round[notfull] .= never
+        # Compute the round of the row/col of a board that was filled first:
+        vec(minimum(round, dims=3-d))
+    end
+    never = length(drawn) + 1
+    first_col_filled = round_filled(1, never)
+    first_row_filled = round_filled(2, never)
+    won = min.(first_col_filled, first_row_filled)
+
+    # Check that all boards won eventually:
+    @assert all(<(never), won)
+
+    # Select board that won last and compute its score:
+    round, b = findmax(won)
+    board = boards[:,:,b]
+    nomarks = marked[:,:,b] .> round
+    s = sum(board[nomarks])
+    num = drawn[round]
+    return b, num, s, s*num
 end
 
 _drawn, _boards = load("test.txt")
 drawn, boards = load("input.txt")
 
-@test bingo(_drawn, _boards) == (188, 24, 4512)
-@show bingo(drawn, boards)
+@test first_bingo(_drawn, _boards) == (3, 24, 188, 4512)
+@show first_bingo(drawn, boards)
+
+@test last_bingo(_drawn, _boards) == (2, 13, 148, 1924)
+@show last_bingo(drawn, boards)
