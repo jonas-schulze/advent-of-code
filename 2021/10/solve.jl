4dc8878f404051ddccbc3d51190245792dd396bf
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-12-15 13:46:55 +0100

    Solve day 10b

--- 2021/10/solve.jl
+++ 2021/10/solve.jl
@@ -25,18 +25,17 @@ function parseline(line)
         delims_match(o, c) || return corrupt, c
     end
     isempty(stack) && return ok, nothing
-    return incomplete, nothing
+    return incomplete, stack
 end
-
-const SCORE = Dict(
-    ')' => 3,
-    ']' => 57,
-    '}' => 1197,
-    '>' => 25137,
-)
 
 function score_corrupted(file)
     s = 0
+    SCORE = Dict(
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137,
+    )
     for l in eachline(file)
         status, c = parseline(l)
         status == corrupt || continue
@@ -45,5 +44,33 @@ function score_corrupted(file)
     s
 end
 
+function score_incomplete(file)
+    s = Int[]
+    SCORE = Dict(
+        '(' => 1,
+        '[' => 2,
+        '{' => 3,
+        '<' => 4,
+    )
+    for l in eachline(file)
+        status, stack = parseline(l)
+        status == incomplete || continue
+        _s = 0
+        reverse!(stack)
+        for o in stack
+            _s *= 5
+            _s += SCORE[o]
+        end
+        push!(s, _s)
+    end
+    sort!(s)
+    n = length(s)
+    m = 1 + n÷2
+    s[m]
+end
+
 @test score_corrupted("test.txt") == 26397
 @show score_corrupted("input.txt")
+
+@test score_incomplete("test.txt") == 288957
+@show score_incomplete("input.txt")
