b795031f80bdf97ab0a2d62bcfd796f1a6360f17
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-12-16 13:24:17 +0100

    Solve day 12b

--- 2021/12/solve.jl
+++ 2021/12/solve.jl
@@ -15,28 +15,49 @@ end
 
 issmall(cave::String) = islowercase(first(cave))
 
-explore(adj) = explore(adj, ["start"])
+explore(f, adj, anytwice) = explore(f, adj, anytwice, ["start"])
 
-function explore(adj, path)::Vector{Vector{String}}
+function explore(f, adj, anytwice, path)
     v = last(path)
-    v == "end" && return [path]
-    paths = Vector{String}[]
+    v == "end" && return f(path)
     for w in adj[v]
-        issmall(w) && w in path && continue
-        next = explore(adj, vcat(path, w))
-        append!(paths, next)
+        w == "start" && continue
+        if issmall(w)
+            once = w in path
+            once && anytwice && continue
+            explore(f, adj, anytwice|once, vcat(path, w))
+        else
+            explore(f, adj, anytwice, vcat(path, w))
+        end
     end
-    return paths
+end
+
+function count_paths(adj, allow2=false)
+    n = 0
+    explore(adj, !allow2) do _
+        n += 1
+    end
+    return n
+end
+
+function show_paths(adj, allow2=false)
+    explore(adj, !allow2) do path
+        println(path)
+    end
 end
 
 adj1 = read_graph("test1.txt")
 adj2 = read_graph("test2.txt")
 adj3 = read_graph("test3.txt")
 
-@test length(explore(adj1)) == 10
-@test length(explore(adj2)) == 19
-@test length(explore(adj3)) == 226
+@test count_paths(adj1) == 10
+@test count_paths(adj2) == 19
+@test count_paths(adj3) == 226
+@test count_paths(adj1, true) == 36
+@test count_paths(adj2, true) == 103
+@test count_paths(adj3, true) == 3509
 
 adj = read_graph("input.txt")
 
-@show length(explore(adj))
+@show count_paths(adj)
+@show count_paths(adj, true)
