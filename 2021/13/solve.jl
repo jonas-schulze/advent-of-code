09f56c54402c1775c04e73c3468174a1c9842d51
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-12-17 10:16:53 +0100

    Solve day 13a

---
+++ 2021/13/solve.jl
@@ -0,0 +1,46 @@
+using Test
+
+function read_manual(file)
+    dots = Tuple{Int,Int}[]
+    folds = Tuple{Char,Int}[]
+    open(file) do io
+        # read dots:
+        dot = r"(\d+),(\d+)"
+        for l in eachline(io)
+            isempty(l) && break
+            as, bs = match(dot, l).captures
+            a = parse(Int, as)
+            b = parse(Int, bs)
+            push!(dots, (a, b))
+        end
+        # read fold instructions:
+        fold = r"fold along (.)=(\d+)"
+        for l in eachline(io)
+            xys, fs = match(fold, l).captures
+            xy = first(xys)
+            f = parse(Int, fs)
+            push!(folds, (xy, f))
+        end
+    end
+    return dots, folds
+end
+
+fold1(f, x) = x > f ? (2f - x) : x # spaces around `-` are needed :-(
+fold(::Val{'x'}, f, x, y) = fold1(f, x), y
+fold(::Val{'y'}, f, x, y) = x, fold1(f, y)
+
+function fold_paper!(dots, (xy, f))
+    d = Val(xy)
+    map!(dots, dots) do (x, y)
+        fold(d, f, x, y)
+    end
+end
+
+function count_after_fold(file)
+    dots, folds = read_manual(file)
+    fold_paper!(dots, folds[1])
+    length(Set(dots))
+end
+
+@test count_after_fold("test.txt") == 17
+@show count_after_fold("input.txt")
