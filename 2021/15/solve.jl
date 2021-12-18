70ad75bc125bd7522c2a3b85c00144c988f41075
Jonas Schulze <jonas.schulze@st.ovgu.de>
2021-12-18 14:58:35 +0100

    Solve day 15b

--- 2021/15/solve.jl
+++ 2021/15/solve.jl
@@ -49,8 +49,28 @@ function dijkstra(grid)
     return D[dst]
 end
 
+function repeat5(grid)
+    m, n = size(grid)
+    grid5 = similar(grid, 5m, 5n)
+    # Repeat tiles:
+    for i in 0:4, j in 0:4
+        ti = i*m
+        tj = j*n
+        grid5[ti+1:ti+m,tj+1:tj+n] .= grid .+ (i + j)
+    end
+    # Wrap numbers greater than 9:
+    for i in eachindex(grid5)
+        @inbounds grid5[i] = mod1(grid5[i], 9)
+    end
+    return grid5
+end
+
 test = read_grid("test.txt")
+test5 = repeat5(test)
 @test dijkstra(test) == 40
+@test dijkstra(test5) == 315
 
 input = read_grid("input.txt")
+input5 = repeat5(input)
 @show dijkstra(input)
+@show dijkstra(input5)
